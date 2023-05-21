2c75039162b315de5edc15f15397c28edf1ece72
Keertana Bheemaraju <vamsiperala@Vamsis-MacBook-Pro.local>
2023-05-21 11:57:40 -0400

    setup springboot app

---
+++ src/main/java/com/castlabs/mp4Analyzer/Mp4AnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.castlabs.mp4Analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp4AnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp4AnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/castlabs/mp4Analyzer/Mp4AnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castlabs.mp4Analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mp4AnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
