ffacee197461059587bebd20f6ed5a279230fce5
Keertana Bheemaraju <bkeertana@gmail.com>
2023-05-23 00:37:25 -0400

    cleanup

--- src/main/java/com/castlabs/mp4Analyzer/controllers/MP4Controller.java
+++ src/main/java/com/castlabs/mp4Analyzer/controllers/MP4Controller.java
@@ -1,16 +1,14 @@
 package com.castlabs.mp4Analyzer.controllers;
 
 import com.castlabs.mp4Analyzer.services.Mp4Service;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping(path="/mp4")
@@ -20,7 +18,7 @@ public class MP4Controller {
     Mp4Service mp4Service;
 
     @GetMapping(path = "/analyzeMp4")
-    public @ResponseBody String analyzeMp4(@RequestParam String mp4Url) throws JsonProcessingException {
+    public @ResponseBody String analyzeMp4(@RequestParam String mp4Url) throws IOException {
 
         return mp4Service.analyzeMp4(mp4Url);
 
